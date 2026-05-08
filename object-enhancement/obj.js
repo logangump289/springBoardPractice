efe1d65db1a7b913d435972a49ef6079789e1d0e
logangump <logan.gump6970@gmail.com>
2026-05-07 22:49:05 -0400

    add js file for object enhancements exercise.

---
+++ object-enhancement/obj.js
@@ -0,0 +1,46 @@
+function createInstructor(firstName, lastName){
+  return {
+    firstName: firstName,
+    lastName: lastName
+  }
+}
+
+function createInstructor(firstName, lastName){
+  return {
+    firstName,
+    lastName
+  }
+}
+
+var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt"
+}
+
+instructor[favoriteNumber] = "That is my favorite!"
+
+const instructor = {
+  firstName: "Colt",
+  sayHi(){
+    return "Hi!";
+  },
+  sayBye(){
+    return this.firstName + " says bye!";
+  }
+}
+
+const d = createAnimal("dog", "bark", "Woooof!")
+d.bark()
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+s.bleet()
+
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
